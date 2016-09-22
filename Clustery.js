f8444dfde2f86bb54b1ff00ab208757b2d37aafe
zjzhome <zjzhome@126.com>
2016-09-22 15:41:56 +0800

    init

---
+++ Clustery.js
@@ -0,0 +1,229 @@
+
+/*!
+ * Clustery.js基于Clusterize.js修改而来
+ * Clusterize.js 基于DOM, 参数rows传入列表数组或者库自行根据已有的DOM结构解析
+ * Clustery.js 基于数据的, 参数rows必须传入数组, 不再是操作DOM结构, 而是返回
+ *             操作列表的数据
+ *
+ * @author darkzone
+ */
+
+/*! Clusterize.js - v0.16.1 - 2016-08-16
+* http://NeXTs.github.com/Clusterize.js/
+* Copyright (c) 2015 Denis Lukov; Licensed GPLv3 */
+
+;(function(name, definition) {
+    if (typeof module != 'undefined') module.exports = definition();
+    else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
+    else this[name] = definition();
+}('Clustery', function() {
+  "use strict"
+
+  var is_mac = navigator.platform.toLowerCase().indexOf('mac') + 1;
+
+  var Clustery = function(data) {
+    if( ! (this instanceof Clustery))
+      return new Clustery(data);
+    var self = this;
+
+    var defaults = {
+      item_height: 0,       // 每一个item的高度
+      block_height: 0,      // 每一个块的高度 item_height * rows_in_block
+      rows_in_block: 20,    // 每一个块包含的行数
+      rows_in_cluster: 0,   // 每一个cluster包含的行数 blocks_in_cluster * rows_in_block
+      cluster_height: 0,    // 每一个cluster的高度 block_height * blocks_in_cluster
+      blocks_in_cluster: 4, // 每个cluster包含的blocks的个数
+      scroll_top: 0
+    }
+
+    self.options = {};
+    var options = ['item_height', 'rows_in_block', 'blocks_in_cluster', 'callbacks'];
+    for(var i = 0, option; option = options[i]; i++) {
+      self.options[option] = typeof data[option] != 'undefined' && data[option] != null
+        ? data[option]
+        : defaults[option];
+    }
+
+    var elems = ['scroll', 'content'];
+    for(var i = 0, elem; elem = elems[i]; i++) {
+      self[elem + '_elem'] = data[elem + 'Id']
+        ? document.getElementById(data[elem + 'Id'])
+        : data[elem + 'Elem'];
+      if( ! self[elem + '_elem'])
+        throw new Error("Error! Could not find " + elem + " element");
+    }
+
+    // tabindex forces the browser to keep focus on the scrolling list, fixes #11
+    if( ! self.content_elem.hasAttribute('tabindex'))
+      self.content_elem.setAttribute('tabindex', 0);
+
+    var rows = data.rows,
+      cache = {start: 0, end: 0, bottom: 0},
+      scroll_top = self.scroll_elem.scrollTop;
+
+    // get row height
+    self.exploreEnvironment(rows);
+
+    // append initial data
+    self.notifyData(rows, cache);
+
+    // restore the scroll position
+    self.scroll_elem.scrollTop = scroll_top;
+
+    // adding scroll handler
+    var last_cluster = false,
+    scroll_debounce = 0,
+    pointer_events_set = false,
+    scrollEv = function() {
+      // fixes scrolling issue on Mac #3
+      if (is_mac) {
+          if( ! pointer_events_set) self.content_elem.style.pointerEvents = 'none';
+          pointer_events_set = true;
+          clearTimeout(scroll_debounce);
+          scroll_debounce = setTimeout(function () {
+              self.content_elem.style.pointerEvents = 'auto';
+              pointer_events_set = false;
+          }, 50);
+      }
+      if (last_cluster != (last_cluster = self.getClusterNum()))
+        self.notifyData(rows, cache);
+    },
+    resize_debounce = 0,
+    resizeEv = function() {
+      clearTimeout(resize_debounce);
+      resize_debounce = setTimeout(self.refresh, 100);
+    }
+    on('scroll', self.scroll_elem, scrollEv);
+    on('resize', window, resizeEv);
+
+    // public methods
+    self.update = function(new_rows) {
+      rows = isArray(new_rows)
+        ? new_rows
+        : [];
+      var scroll_top = self.scroll_elem.scrollTop;
+      // fixes #39
+      if(rows.length * self.options.item_height < scroll_top) {
+        self.scroll_elem.scrollTop = 0;
+        last_cluster = 0;
+      }
+      self.notifyData(rows, cache);
+      self.scroll_elem.scrollTop = scroll_top;
+    }
+
+    self.destroy = function(clean) {
+      off('scroll', self.scroll_elem, scrollEv);
+      off('resize', window, resizeEv);
+      var callbacks = this.options.callbacks;
+      callbacks.shouldUpdate({
+        top_offset: 0,
+        bottom_offset: 0,
+        rows_above: 0,
+        start: 0,
+        end: rows.length
+      });
+    }
+
+    self.refresh = function() {
+      self.getRowsHeight(rows) && self.update(rows);
+    }
+  }
+
+  Clustery.prototype = {
+    constructor: Clustery,
+
+    // calc cluster height
+    exploreEnvironment: function(rows) {
+      this.getRowsHeight(rows);
+    },
+
+    getRowsHeight: function(rows) {
+      var opts = this.options;
+      opts.cluster_height = 0;
+      if( ! rows.length) return;
+      
+      opts.block_height = opts.item_height * opts.rows_in_block;
+      opts.rows_in_cluster = opts.blocks_in_cluster * opts.rows_in_block;
+      opts.cluster_height = opts.blocks_in_cluster * opts.block_height;
+      return true
+    },
+
+    // get current cluster number
+    getClusterNum: function () {
+      this.options.scroll_top = this.scroll_elem.scrollTop;
+      return Math.floor(this.options.scroll_top / (this.options.cluster_height - this.options.block_height)) || 0;
+    },
+
+    // generate cluster for current scroll position
+    generate: function (rows, cluster_num) {
+      var opts = this.options,
+        rows_len = rows.length;
+      if (rows_len < opts.rows_in_block) {
+        return {
+          top_offset: 0,
+          bottom_offset: 0,
+          rows_above: 0,
+          start: 0,
+          end: rows_len
+        }
+      }
+      if( ! opts.cluster_height) {
+        this.exploreEnvironment(rows);
+      }
+      var items_start = Math.max((opts.rows_in_cluster - opts.rows_in_block) * cluster_num, 0),
+        items_end = items_start + opts.rows_in_cluster,
+        top_offset = Math.max(items_start * opts.item_height, 0),
+        bottom_offset = Math.max((rows_len - items_end) * opts.item_height, 0),
+        rows_above = items_start;
+      if(top_offset < 1) {
+        rows_above++;
+      }
+
+      return {
+        top_offset: top_offset,
+        bottom_offset: bottom_offset,
+        rows_above: rows_above,
+        start: items_start,
+        end: items_end
+      }
+    },
+
+    // if necessary verify data changed and insert to DOM
+    notifyData: function(rows, cache) {
+      var data = this.generate(rows, this.getClusterNum()),
+        this_cluster_start_changed = this.checkChanges('data', data.start, cache),
+        this_cluster_end_changed = this.checkChanges('data', data.end, cache),
+        only_bottom_offset_changed = this.checkChanges('bottom', data.bottom_offset, cache),
+        callbacks = this.options.callbacks;
+
+      if(this_cluster_start_changed || this_cluster_end_changed) {
+        callbacks.shouldUpdate(data);
+        // this.options.content_tag == 'ol' && this.content_elem.setAttribute('start', data.rows_above);
+      } else if(only_bottom_offset_changed) {
+        callbacks.shouldUpdate(data.bottom_offset)
+      }
+    },
+
+    checkChanges: function(type, value, cache) {
+      var changed = value != cache[type];
+      cache[type] = value;
+      return changed;
+    }
+  }
+
+  // support functions
+  function on(evt, element, fnc) {
+    return element.addEventListener ? element.addEventListener(evt, fnc, false) : element.attachEvent("on" + evt, fnc);
+  }
+  function off(evt, element, fnc) {
+    return element.removeEventListener ? element.removeEventListener(evt, fnc, false) : element.detachEvent("on" + evt, fnc);
+  }
+  function isArray(arr) {
+    return Object.prototype.toString.call(arr) === '[object Array]';
+  }
+  function getStyle(prop, elem) {
+    return window.getComputedStyle ? window.getComputedStyle(elem)[prop] : elem.currentStyle[prop];
+  }
+
+  return Clustery
+}))
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Clustery.js
-based on Clusterize.js, displaying large data easily.
+
+基于[Clusterize.js](http://clusterize.js.org/)，适合于MVVM框架展示超长列表。
+
+## 写在前面
+
+对于长列表，一般的应用可以通过分页解决。然而现在很多的应用列表部分可能是滚动加载的，随着滚动，列表项越来越多，影响性能，尤其是移动设备上；另一种是IM应用，像会话列表、好友列表、群成员列表通常会一次性加载。在这些情况下，对于长列表的优化就显得很有必要。
+
+[Clusterize.js](http://clusterize.js.org/)就是这样一个库，短小精悍。它会将列表划分成一个个的cluster，随着滚动显示可见的cluster，并在列表的顶部和底部填充额外的高度，展示列表的真实高度。
+
+不过Clusterize.js是基于DOM的，随着MVVM框架的流行，大家更多的是在操作数据，所以基于Clusterize.js做了Clustery.js。
+
+## 如何使用
+
+参数和Clusterize.js基本一致，不过rows此时是必须传入的，不再是DOM列表，而是要渲染列表的数组。另外增加一个参数itemHeight，必须显示的指定每一项的高度。
+
+callbacks只有一个回调：shouldUpdate，在此回调里得到data，更新UI。data数据结构如下：
+```js
+{
+    start: 0,   // 渲染列表从start到end, 此时需要渲染arr.slice(start, end)   
+    end: 80,
+    top_offset: 1245,   // 列表顶部填充高度
+    bottom_offset: 3349 // 列表底部填充高度
+}
+```
+
+列表数据源更新后, 记得update一下~
+
+下面是所有的配置项、回调和公共方法：
+
+```js
+this.clustery = new Clustery({
+    scrollElem: scrollElem,
+    contentElem: contentElem,
+    rows: rows,
+    item_height: itemHeight,
+    rows_in_block: 20,
+    blocks_in_cluster: 4,
+    callbacks: {
+        shouldUpdate: function(data) {
+            _this.setRenderData(data)
+        }
+    }
+});
+
+// if you want to destroy
+this.clustery.destroy();
+
+// if you update data
+this.clustery.update(newRows);
+```
+
+## 缺陷
+
+* 每一项的高度要固定，这样就不能用于IM消息列表中。
+* 数据源限定为了数组。
+
+虽然如此，他已经能解决大部分的需求了。
